05ee5614d620f91690ebab0a8a4d29c44caed550
gyansahoo666 <119865696+gyansahoo666@users.noreply.github.com>
2022-12-23 03:40:49 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,15 +5,15 @@ printf "\n======================================================================
 printf "KOBITON EXECUTE TEST ACTION"
 printf "\n=====================================================================================\n\n"
 
-# -change the name of app to run based on your system (app_darwin for macOS, app_linux for Linux, app_windows for Windows)
-TARGET_OS="Windows"
+# change the name of app to run based on your system (app_darwin for macOS, app_linux for Linux, app_windows for Windows)
+TARGET_OS="linux"
 echo "Runner OS: ${RUNNER_OS}"
 if [ $RUNNER_OS == "macOS" ]; then
     TARGET_OS="darwin"
 elif [ $RUNNER_OS == "Windows" ]; then
     TARGET_OS="windows"
 fi
-APP_TO_RUN="app_${TARGET_OS}"
+APP_TO_RUN="app_Windows}"
 chmod +x $GITHUB_ACTION_PATH/app-to-run/${APP_TO_RUN}
 
 $GITHUB_ACTION_PATH/app-to-run/${APP_TO_RUN}
